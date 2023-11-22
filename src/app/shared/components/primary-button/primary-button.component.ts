116f3c1ebb978dbbc733d73f0211455f4e2e79ad
Itallo <italloazevedo@hotmail.com>
2023-11-22 20:32:26 -0300

    feat: primary button component

---
+++ src/app/shared/components/primary-button/primary-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  templateUrl: './primary-button.component.html',
+  styleUrl: './primary-button.component.css'
+})
+export class PrimaryButtonComponent {
+
+}
