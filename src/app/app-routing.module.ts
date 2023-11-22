01cf4ac51ad24f263350715fb7a335044ed80858
Itallo <italloazevedo@hotmail.com>
2023-11-22 20:37:49 -0300

    feat: added lazy routing and shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'replace-text', loadChildren: () => import('./features/replace-text/replace-text.module').then(m => m.ReplaceTextModule) },
+  {
+    path: '',
+    redirectTo: 'replace-text',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
