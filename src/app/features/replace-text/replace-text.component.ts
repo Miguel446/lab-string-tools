d184d32c707625c46f4442cfd68bddd7d097960b
Itallo <italloazevedo@hotmail.com>
2023-11-23 19:24:26 -0300

    feat(replace-text): added parameters for text area

--- src/app/features/replace-text/replace-text.component.ts
+++ src/app/features/replace-text/replace-text.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ReplaceTextComponent {
 
+  handleTextOutput(text: string) {
+    console.log("Event: " + text);
+  }
+
+  replaceText() {
+    // TODO
+  }
+
 }
